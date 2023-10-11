e4e13fa56a6cf4921e3568bca8b29f2edd7e5912
hotvely <hwan_o3o@naver.com>
2023-10-11 10:17:53 +0900

    feat : 2023_10_11 freemarket, hr, vi 관련 클래스 삭제

--- src/main/java/com/kh/Freepets/repo/board/freeMarket/FreeMarketDAO.java
+++
@@ -1,7 +0,0 @@
-package com.kh.Freepets.repo.board.freeMarket;
-
-import com.kh.Freepets.domain.board.freeMarket.FreeMarket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FreeMarketDAO extends JpaRepository<FreeMarket, Integer> {
-}
